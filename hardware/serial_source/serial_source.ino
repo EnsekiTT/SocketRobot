e5e1063150ff341a9594d4d1e551cd85684af2db
EnsekiTT <ensekitt@curbsci.com>
2015-07-08 22:17:50 +0900

    edit: serial_source.inoからサーボを扱えるようにした

--- hardware/serial_source/serial_source.ino
+++ hardware/serial_source/serial_source.ino
@@ -1,14 +1,19 @@
-int analogValue;
+#include <Servo.h>
+
+double analogValue;
+int angle;
+Servo servo1;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  servo1.attach(9);
+  servo1.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogValue = analogRead(0);
-  
-  Serial.println(analogValue);
-  delay(10);
+  analogValue = double(analogRead(0));
+  angle = int(analogValue/1024.0 * 180);
+  servo1.write(angle);
+  Serial.println(angle);
 }
