fd26f6062512fef27b446fe578510119569ca184
EnsekiTT <ensekitt@curbsci.com>
2015-07-22 23:26:19 +0900

    add audio analyzer source and database connector source(trial)

--- hardware/serial_source/serial_source.ino
+++ hardware/serial_source/serial_source.ino
@@ -1,19 +1,36 @@
 #include <Servo.h>
+#include <Wire.h>
+//https://github.com/sparkfun/MPU-9150_Breakout
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
 double analogValue;
 int angle;
 Servo servo1;
 
+MPU6050 accelgyro; // address = 0x68, the default, on MPU6050 EVB
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t mx, my, mz;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  servo1.attach(9);
-  servo1.write(0);
+  Serial.begin(38400);
+  //servo1.attach(9);
+  //servo1.write(0);
+  
+  Wire.begin();
+  accelgyro.initialize();
+  accelgyro.setI2CBypassEnabled(true);
+  
 }
 
 void loop() {
+  accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+  
   analogValue = double(analogRead(0));
   angle = int(analogValue/1024.0 * 180);
-  servo1.write(angle);
-  Serial.println(angle);
+  //servo1.write(angle);
+  Serial.println(ax);
 }
