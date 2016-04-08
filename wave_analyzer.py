7edc3599de0198f77cc02359f773f3cd10f9f094
EnsekiTT <ensekitt@gmail.com>
2016-04-08 20:31:41 +0900

    Define functions

--- wave_analyzer.py
+++ wave_analyzer.py
@@ -7,11 +7,53 @@ Read wave date and analyze it. (wave is not only audio wave)
 __auther__ = 'EnsekiTT'
 __version__ = '0.1'
 
+import pyaudio
+import numpy as np
+
 class SocketRobotWave():
     def __init__(self):
         print 'SocketRobotWave'
+        self.p = pyaudio.pyaudio()
+
+    def getDevices(self):
+        self.deviceCount = self.pyaud.get_device_count()
+        self.deviceInfo = self.pyaud.get_device_info_by_index()
+
+    # show device information
+    def showDevices(self):
+        pass
+    
+    # set audio stream by device IDs
+    def setStream(self, deviceIds):
+        pass
+
+    # get audio stream info
+    def getStream(self):
+        pass
+
+    # record while n[sec]. 0 is inf
+    def recordStream(self, n):
+         pass
+
+    # listen while n[sec]. 0 is inf
+    def listenStream(self, n):
+         pass
+
+    # set file format
+    def setFileFormat(self, str):
+         pass
+
+    # callback
+    def callback(in_data, frame_count, time_info, status):
+        return (in_data, pyaudio.paContinue)
+    
+    # filter
+    def filter(self, in_data):
+        out_data = in_data
+        return out_data
+
+    def iirFilter(self, 
 
 
 if __name__ == '__main__':
     srw = SocketRobotWave()
-    
